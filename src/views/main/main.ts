8fa82da4347554bf356e6e02af7d6a77d1ecb341
Ginter Chen <ginter_chen@asus.com>
2023-09-05 21:44:53 +0800

    add mock data

--- src/views/main/main.ts
+++ src/views/main/main.ts
@@ -1,22 +1,21 @@
-
+//import
+import { reactive, watch, onMounted, ref } from "vue"
+import datas from "../../../public/mock/data.json"
+//start
 export default {
   name: 'main',
-  components: {},
-  props: [],
-  data () {
+  components: {
+
+  },
+  props: {
+
+  },
+  setup() {
+    let state = reactive({})
+    onMounted(async () => {
+    });
+    
     return {
-
     }
-  },
-  computed: {
-
-  },
-  mounted () {
-
-  },
-  methods: {
-
   }
 }
-
-
