77745ed6913556e7db09c4f405b27bfa90430f34
Ginter Chen <ginter_chen@asus.com>
2023-09-06 14:04:45 +0800

    add select

--- src/components/menuItem/menuItem.ts
+++ src/components/menuItem/menuItem.ts
@@ -14,10 +14,15 @@ export default {
       type: Number,
       default: 0,
     },
+    selectData: {
+      type: String,
+      default: "",
+    },
   },
   setup(props) {
     let openItems = ref([]);
     onMounted(async () => {
+      openItems.value = [findFather(props.menuData,props.selectData)];
     });
     function toggleItem(item) {
       if (isOpen(item)) {
@@ -26,7 +31,6 @@ export default {
       } else {
         // 如果未展開，則展開並收合其他已展開的項目
         openItems.value = [item.key];
-        
       }
     }
     function isOpen(item) {
@@ -39,6 +43,27 @@ export default {
       if(props.level!=0)return false
       return openItems.value.includes(item.key);
     }
+    function findFather(data, keyToFind) {
+      for (const item of data) {
+        if (item.key === keyToFind) {
+          return keyToFind; // 如果找到匹配的 key，返回它自己
+        }
+    
+        if (item.children) {
+          const result = findFather(item.children, keyToFind);
+          if (result) {
+            return item.key; // 如果在子层中找到匹配的 key，返回当前层级的 key
+          }
+        }
+      }
+    
+      return null; // 如果未找到匹配的 key，返回 null 或其他适当的值
+    }
+    watch(
+      () => props.selectData,
+      (val) => {
+        openItems.value = [findFather(props.menuData,val)];
+      })
     return {
       props,
       toggleItem,
--- src/views/main/main.ts
+++ src/views/main/main.ts
@@ -1,5 +1,5 @@
 //import
-import { reactive, watch, onMounted, ref } from "vue"
+import { reactive, watch, onMounted, ref, toValue } from "vue"
 import datas from "../../../public/mock/data.json"
 import menuItem from "@/components/menuItem/index.vue"
 //start
@@ -14,13 +14,13 @@ export default {
   setup() {
     let start = ref(false);
     let showPopup = ref(false);
-    let balls = reactive([
-      // {left:50,top:50},
-      // {left:310,top:50},
-      // {left:50,top:600},
-      // {left:310,top:600},
-    ])
+    let balls = reactive([])
+    let flattenedData = ref([])
+    let selectData = ref("")
     onMounted(async () => {
+      //扁平化資料
+      flattenedData.value = flattenData(datas);
+      //隨機創造100個球
       for(var a = 0;a<=99;a++){
         balls.push({left:getRandomInt(window.innerWidth),top:getRandomInt(window.innerHeight)});
       }
@@ -32,13 +32,25 @@ export default {
     function getRandomInt(max) {
       return Math.floor(Math.random() * max);
     }
-    
+    function flattenData(data) {
+      let result = [];
+      data.forEach(item => {
+        const { children, ...rest } = item;
+        result.push(rest);
+        if (children) {
+          result = result.concat(flattenData(children));
+        }
+      });
+      return result;
+    }
     return {
       showPopup,
       clickMenu,
       datas,
       start,
       balls,
+      selectData,
+      flattenedData,
     }
   }
 }
